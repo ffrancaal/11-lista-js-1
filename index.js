4b39d5ca845caeb771c9fa28f70ad1bfda0eb32f
ffrancaal <francafelipe.al@gmail.com>
2025-11-06 20:52:03 -0300

    Implement multiple mathematical functions
    Added functions for addition, division, area calculation, parity check, and square root.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 /**** Escreva o código abaixo 👇******/
 
+// Questão 1
+function somar(a, b) {
+  return a + b;
+}
 
+//Questão 4
+function dividir(c, d) {
+  if (d === 0) {
+    throw new Error("Erro: divisão por zero");
+  }
+  return c/ d;
+}
 
+// Questão 7
+function calcularAreaRetangulo(largura, altura) {
+  return largura * altura;
+}
 
+//Questão 8
+function verificarParOuImpar(numero) {
+  if (numero % 2 === 0) {
+    return "par";
+  } else {
+    return "ímpar";
+  }
+}
 
-
-
+//Questão 9
+function calcularRaizQuadrada(numero) {
+  return Math.sqrt(numero);}
 
 /**** Escreva o código acima ☝️******/
 
